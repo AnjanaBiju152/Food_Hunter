4f895293b8ad424a1da6245cc873a6a434640427
AnjanaBiju152 <anjana.biju200@gmail.com>
2024-08-13 10:44:30 +0530

    changes

--- App.js
+++ App.js
@@ -1,16 +1,75 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const p = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "Hello I am heading 1 from React"),
-    React.createElement("h2", {}, "Hello I am heading 2 from React"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "Hello I am heading 1 from React"),
-    React.createElement("h2", {}, "Hello I am heading 2 from React"),
-  ]),
-]);
+
+
+const container = React.createElement(
+  "h1",
+  { id: "heading1" },
+  "React"
+);
+console.log(container);
+
+const jsxConatiner = (
+  <h1 className="heading1">React using jsx</h1>
+);
+const HeadingComponents = () => (
+  <div id="container">
+    <h1>React functional component</h1>;
+  </div>
+);
+
+const WithoutReturn = () => (
+  <div>
+    <h1>I am here without Return</h1>
+  </div>
+)
+
+const WithReturn = () => {
+  return (
+    <div>
+      <h1>I am here with Return</h1>
+    </div>
+  )
+}
+
+const reactElement = (
+  <h1>I am a React Element</h1>
+)
+
+const Component = () => { 
+  return(
+  <div id="container1">
+    <h1> component composition </h1>
+  </div>
+)}
+const ReactComponent = () => (
+  <div id="container2">
+  <Component/>
+    <h1>I am a React component</h1>
+  </div>
+)
+
+
+
+const reactEle = (
+  <div>
+  {reactElement}
+    <h1>React Element</h1>
+  </div>
+)
+const ReactComp = () => {
+  return (
+    <div>
+      
+      {ReactComponent()}
+      {<h1>Heading inside curly braces</h1>}
+      <h1>{reactEle} inside React Component</h1>
+      {`I am any Javascript expression ${2+2}`}
+    </div>
+  )
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(p);
+root.render(<ReactComp/>);
---
+++ README.md
@@ -0,0 +1,23 @@
+# Food Hunter
+
+
+
+# parcel
+
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement
+- File Watching Algorithm - Written in C++
+- Caching - Faster Builds
+- Image optimization
+- Minification
+- Bundling
+- Compress
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - Support older browsers
+- Diagnostic
+- Error Handling
+- HTTPs
+- Tree Shaking - remove unused code
+- Different Dev anad Prod Bundles
